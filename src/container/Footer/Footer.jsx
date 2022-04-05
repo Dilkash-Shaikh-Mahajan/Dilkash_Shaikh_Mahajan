f5860a47a99a7030dc2fc552e81c47322aee2777
dilkash7 <dilkash.3mindsdigital@gmail.com>
2022-04-05 15:53:31 +0530

    resolve the lint issues

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -6,7 +6,11 @@ import { client } from '../../client';
 import './Footer.scss';
 
 const Footer = () => {
-  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
 
@@ -14,7 +18,10 @@ const Footer = () => {
 
   const handleChangeInput = (e) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   };
 
   const handleSubmit = () => {
@@ -27,60 +34,52 @@ const Footer = () => {
       message: formData.message,
     };
 
-    client.create(contact)
-      .then(() => {
-        setLoading(false);
-        setIsFormSubmitted(true);
-      })
-      .catch((err) => console.log(err));
+    client.create(contact).then(() => {
+      setLoading(false);
+      setIsFormSubmitted(true);
+    });
   };
 
   return (
     <>
-      <h2 className="head-text">Take a coffee & chat with me</h2>
-
+      <h2 className="head-text"> Take a coffee & chat with me </h2>
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:hello@micael.com" className="p-text">hello@micael.com</a>
-        </div>
+          <a href="mailto:hello@micael.com" className="p-text">
+            hello @micael.com{' '}
+          </a>{' '}
+        </div>{' '}
         <div className="app__footer-card">
           <img src={images.mobile} alt="phone" />
-          <a href="tel:+1 (123) 456-7890" className="p-text">+1 (123) 456-7890</a>
-        </div>
-      </div>
+          <a href="tel:+1 (123) 456-7890" className="p-text">
+            +1(123) 456 - 7890{' '}
+          </a>{' '}
+        </div>{' '}
+      </div>{' '}
       {!isFormSubmitted ? (
         <div className="app__footer-form app__flex">
           <div className="app__flex">
-            <input className="p-text" type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} />
-          </div>
+            <input className="p-text" type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} />{' '}
+          </div>{' '}
           <div className="app__flex">
-            <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} />
-          </div>
+            <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} />{' '}
+          </div>{' '}
           <div>
-            <textarea
-              className="p-text"
-              placeholder="Your Message"
-              value={message}
-              name="message"
-              onChange={handleChangeInput}
-            />
-          </div>
-          <button type="button" className="p-text" onClick={handleSubmit}>{!loading ? 'Send Message' : 'Sending...'}</button>
+            <textarea className="p-text" placeholder="Your Message" value={message} name="message" onChange={handleChangeInput} />{' '}
+          </div>{' '}
+          <button type="button" className="p-text" onClick={handleSubmit}>
+            {' '}
+            {!loading ? 'Send Message' : 'Sending...'}{' '}
+          </button>{' '}
         </div>
       ) : (
         <div>
-          <h3 className="head-text">
-            Thank you for getting in touch!
-          </h3>
+          <h3 className="head-text">Thank you for getting in touch!</h3>{' '}
         </div>
-      )}
+      )}{' '}
     </>
   );
 };
 
-export default AppWrap(
-  MotionWrap(Footer, 'app__footer'),
-  'contact',
-  'app__whitebg',
-);
+export default AppWrap(MotionWrap(Footer, 'app__footer'), 'contact', 'app__whitebg');
